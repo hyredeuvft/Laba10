cbe9e2968dc1175da49c40e30d087cb8bb45a56c
Студент <student7@iMac7.local>
2022-11-18 12:12:07 +0300

    qwerty

---
+++ app/src/main/java/com/example/laba103isp9_14/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.laba103isp9_14;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity  {
+
+
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
